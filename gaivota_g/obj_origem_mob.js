1213bff82ce3766fb4c56b528cf418f5cc6bfd90
guilhermedesennfy-spec <guilherme.desen.nfy@gmail.com>
2026-02-08 19:24:55 -0300

    Update obj_origem_mob.js

--- gaivota_g/obj_origem_mob.js
+++ gaivota_g/obj_origem_mob.js
@@ -216,7 +216,7 @@ function gameLoop() {
     placar.textContent =
         "Gaivota1: " + gaivota.pontos + " | Gaivota2: " + gaivota2.pontos;
 
-    //requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
 // ======= Inicialização =======
@@ -227,7 +227,8 @@ function initGame() {
     //gaivota2.rescale();
     //peixe.rescale();
 
-    requestAnimationFrame(gameLoop);
+    //requestAnimationFrame(gameLoop);
+    gameLoop()
 }
 
 
