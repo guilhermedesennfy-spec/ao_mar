687b5735f89c5ef92f07561ed24b3f8fe896578c
guilhermedesennfy-spec <guilherme.desen.nfy@gmail.com>
2026-02-08 17:23:01 -0300

    Update obj_origem_mob.js

--- gaivota_g/obj_origem_mob.js
+++ gaivota_g/obj_origem_mob.js
@@ -1,23 +1,23 @@
 // ======= Configuração base =======
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let isMobile = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 
-const BASE_WIDTH = 1920;
-const BASE_HEIGHT = 1080;
+let BASE_WIDTH = 1920;
+let BASE_HEIGHT = 1080;
 
 let scale = 1;
 
 // Placar
-const placar = document.querySelector("h3") || { textContent: "" };
+let placar = document.querySelector("h3") || { textContent: "" };
 
 // ======= Ajuste do canvas =======
 function ajustarCanvas() {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
+    let w = window.innerWidth;
+    let h = window.innerHeight;
 
-    const isPortrait = h > w;
-    const warning = document.getElementById("rotate-warning");
+    let isPortrait = h > w;
+    let warning = document.getElementById("rotate-warning");
 
     if (isMobile && isPortrait) {
         warning.style.display = "flex";
@@ -33,78 +33,77 @@ function ajustarCanvas() {
 
     scale = Math.min(w / BASE_WIDTH, h / BASE_HEIGHT);
 
-    if (bg?.rescale) bg.rescale();
-    if (bg2?.rescale) bg2.rescale();
-    if (gaivota?.rescale) gaivota.rescale();
-    if (gaivota2?.rescale) gaivota2.rescale();
-    if (peixe?.rescale) peixe.rescale();
+    bg.rescale();
+    bg2.rescale();
+    gaivota.rescale();
+    gaivota2.rescale();
+    peixe.rescale();
 }
+
 window.addEventListener("resize", ajustarCanvas);
 ajustarCanvas();
 
 // ======= Função degrau =======
 function degrau(x) {
-    const e = Math.E ** (x * 0.2);
-    const k = -((((x * 0.2) - 10) / e) ** 2);
+    let e = Math.E ** (x * 0.2);
+    let k = -((((x * 0.2) - 10) / e) ** 2);
     return (Math.E ** k) * 1.8801;
 }
 
-// ======= Construtor Obj =======
-function Obj(imageSrc, baseX, baseY, baseW = 64, baseH = 48) {
-    this.image = new Image();
-    this.image.src = imageSrc;
+// ======= Factory Function para Objetos =======
+function criarObj(imageSrc, baseX, baseY, baseW = 64, baseH = 48) {
+    let image = new Image();
+    image.src = imageSrc;
 
-    this.baseX = baseX;
-    this.baseY = baseY;
-    this.baseW = baseW;
-    this.baseH = baseH;
+    let obj = {
+        image,
+        baseX,
+        baseY,
+        baseW,
+        baseH,
+        position: [baseX, baseY],
+        width: baseW,
+        height: baseH,
+        frame: 1,
+        tick: 0,
+        pontos: 0,
+        velocidade: 0,
+        x: 2,
 
-    this.position = [baseX, baseY];
-    this.width = baseW;
-    this.height = baseH;
+        rescale() {
+            this.position[0] = this.baseX * scale;
+            this.position[1] = this.baseY * scale;
+            this.width = this.baseW * scale;
+            this.height = this.baseH * scale;
+        },
 
-    this.frame = 1;
-    this.tick = 0;
-    this.pontos = 0;
-    this.velocidade = 0;
-    this.x = 2;
+        drawing(ctx) {
+            ctx.drawImage(this.image, this.position[0], this.position[1], this.width, this.height);
+        },
 
-    this.rescale = function () {
-        this.position[0] = this.baseX * scale;
-        this.position[1] = this.baseY * scale;
-        this.width = this.baseW * scale;
-        this.height = this.baseH * scale;
-    };
+        anim(prefix, tickLimit, frames) {
+            this.tick++;
+            if (this.tick >= tickLimit) {
+                this.tick = 0;
+                this.frame++;
+                if (this.frame > frames) this.frame = 1;
+                this.image.src = prefix + this.frame + ".png";
+            }
+        },
 
-    this.drawing = function (ctx) {
-        ctx.drawImage(this.image, this.position[0], this.position[1], this.width, this.height);
-    };
-
-    this.anim = function (imgPrefix, tickLimit, frames) {
-        this.tick++;
-        if (this.tick >= tickLimit) {
-            this.tick = 0;
-            this.frame++;
-            if (this.frame > frames) this.frame = 1;
-            this.image.src = imgPrefix + this.frame + ".png";
+        move(x, y) {
+            this.position[0] = x - this.width / 2;
+            this.position[1] = y - this.height / 2;
         }
     };
 
-    this.move_gaivota = function (x, y) {
-        this.position[0] = x - this.width / 2;
-        this.position[1] = y - this.height / 2;
-    };
-
-    this.move_gaivota2 = function (x, y) {
-        this.position[0] = x - this.width / 2;
-        this.position[1] = y - this.height / 2;
-    };
+    return obj;
 }
 
 // ======= Movimento do fundo =======
 function move_bg(bg, bg2) {
-    const step = 1 * scale;
-    const limit = 1920 * scale;
+    let step = 1 * scale;
+    let limit = 1920 * scale;
 
     bg.position[0] -= step;
     bg2.position[0] -= step;
@@ -114,8 +113,8 @@ function move_bg(bg, bg2) {
 }
 
 // ======= Movimento e colisões dos peixes =======
-function move_peixe(peixe, gaivota, gaivota2) {
-    peixe.velocidade = (2 + (gaivota.pontos + gaivota2.pontos) / (2 * peixe.x)) * scale;
+function move_peixe(peixe, g1, g2) {
+    peixe.velocidade = (2 + (g1.pontos + g2.pontos) / (2 * peixe.x)) * scale;
 
     peixe.position[1] -= peixe.velocidade;
 
@@ -124,7 +123,7 @@ function move_peixe(peixe, gaivota, gaivota2) {
         peixe.position[1] = 750 * scale;
     }
 
-    const hit = 36 * scale;
+    let hit = 36 * scale;
 
     function colisao(g) {
         return (
@@ -135,27 +134,27 @@ function move_peixe(peixe, gaivota, gaivota2) {
         );
     }
 
-    if (colisao(gaivota)) {
+    if (colisao(g1)) {
         peixe.position[0] = Math.random() * (BASE_WIDTH * scale);
         peixe.position[1] = 750 * scale;
-        gaivota.pontos++;
+        g1.pontos++;
     }
 
-    if (colisao(gaivota2)) {
+    if (colisao(g2)) {
         peixe.position[0] = Math.random() * (BASE_WIDTH * scale);
         peixe.position[1] = 750 * scale;
-        gaivota2.pontos++;
+        g2.pontos++;
     }
 
     if (peixe.velocidade >= 15 * scale) peixe.x++;
 }
 
 // ======= Objetos =======
-const bg = new Obj("img_fundo/fundo2.png", 0, 0, 1920, 1080);
-const bg2 = new Obj("img_fundo/fundo2.png", 1920, 0, 1920, 1080);
-const gaivota = new Obj("img_gaivota/gaivota1.png", 100, 200, 120, 80);
-const gaivota2 = new Obj("img_gaivota2/gaivota1.png", 400, 200, 120, 80);
-const peixe = new Obj("img_peixe/peixe1.png", Math.random() * 774, 750, 64, 48);
+let bg = criarObj("img_fundo/fundo2.png", 0, 0, 1920, 1080);
+let bg2 = criarObj("img_fundo/fundo2.png", 1920, 0, 1920, 1080);
+let gaivota = criarObj("img_gaivota/gaivota1.png", 100, 200, 120, 80);
+let gaivota2 = criarObj("img_gaivota2/gaivota1.png", 400, 200, 120, 80);
+let peixe = criarObj("img_peixe/peixe1.png", Math.random() * 774, 750, 64, 48);
 
 // ======= Entrada do usuário =======
 let destino = null;
@@ -165,7 +164,7 @@ canvas.addEventListener("mousemove", (e) => {
 });
 
 canvas.addEventListener("touchmove", (e) => {
-    const t = e.touches[0];
+    let t = e.touches[0];
     destino = { x: t.clientX, y: t.clientY };
     e.preventDefault();
 });
@@ -178,14 +177,14 @@ function gameLoop() {
     bg2.drawing(ctx);
 
     if (destino) {
-        gaivota.move_gaivota(destino.x, destino.y);
+        gaivota.move(destino.x, destino.y);
     }
 
     if (peixe.position[1] <= 560 * scale) {
-        const fatorX = 0.0015 + (gaivota.pontos + gaivota2.pontos) / 3500;
-        const fatorY = 0.0002 + (gaivota.pontos + gaivota2.pontos) / 2000;
+        let fatorX = 0.0015 + (gaivota.pontos + gaivota2.pontos) / 3500;
+        let fatorY = 0.0002 + (gaivota.pontos + gaivota2.pontos) / 2000;
 
-        gaivota2.move_gaivota2(
+        gaivota2.move(
             peixe.position[0] * fatorX + gaivota2.position[0],
             peixe.position[1] * fatorY + gaivota2.position[1]
         );
@@ -218,4 +217,5 @@ function initGame() {
 
     requestAnimationFrame(gameLoop);
 }
-initGame()
+
+initGame();
