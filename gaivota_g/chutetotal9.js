193f72c31c734c31c3cf5de3ee2716baccde51de
guilhermedesennfy-spec <guilherme.desen.nfy@gmail.com>
2026-03-01 17:50:19 -0300

    Update chutetotal9.js

--- gaivota_g/chutetotal9.js
+++ gaivota_g/chutetotal9.js
@@ -19,7 +19,7 @@ const H = canvas.height;
 const scaleX = W / BASE_W;
 const scaleY = H / BASE_H;
 
-const scale = (Math.min(scaleX, scaleY));
+const scale = (Math.min(scaleX, scaleY))/1;
 
 // ======= FUNÇÃO DEGRAU ORIGINAL =======
 function degrau(x){
